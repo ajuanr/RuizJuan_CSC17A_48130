e077bcb8dab0b15f176817b43628bfc529542e6b
juan <j_ruiz05@yahoo.com>
2014-09-30 09:37:41 -0700

    Signed-off-by: juan <juan@juan.com>

--- jr2250728/Hmwk/Assignment 2/main.cpp
+++ jr2250728/Hmwk/Assignment 2/main.cpp
@@ -6,8 +6,13 @@
  */
 
 #include <cstdlib>
+#include <iostream>
+#include <string>
 
 using namespace std;
+
+void problem10_12();
+bool isValid(string);
 
 /*
  * 
@@ -17,3 +22,29 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+/*
+* The password should be at least six characters long.
+* The password should contain at least one uppercase and
+* at least one lowercase letter.
+* The password should have at least one digit.
+ */
+void problem10_12() {
+    string pswd;
+    do {
+    cout << "Enter password\n"
+            "Password should be at least six characters long.\n"
+            "The password should contain at least one uppercase and
+            " at least one lowercase letter.\n"
+            "The password should have at least one digit.\n";
+    cin >> pswd;
+    } while (!isValid(pswd));
+}
+
+bool isValid(string pswd) {
+    bool length = false;    // password must be six digits
+    bool upper = false;    // contain at least one uppercase character
+    bool lower = false;    // contain at least one lowercase character
+    bool digit = false;    // contain at least one digit
+    
+    
+}
