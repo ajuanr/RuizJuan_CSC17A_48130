1730f5e36facbb3e27beef9de845da288b92bbca
juan <j_ruiz05@yahoo.com>
2014-09-30 10:15:34 -0700

    Signed-off-by: juan <juan@juan.com>

--- jr2250728/Hmwk/Assignment 2/main.cpp
+++ jr2250728/Hmwk/Assignment 2/main.cpp
@@ -7,6 +7,7 @@
  */
 
 #include <cstdlib>
+#include <iomanip>
 #include <iostream>
 #include <string>
 
@@ -15,12 +16,15 @@ using namespace std;
 void prob10_12();
 void prob10_19();
 bool isValid(string);
+void formatCheck(string, string, float);
+string numToStr(float);
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    problem10_12();
+    //prob10_12();
+    prob10_19();
     return 0;
 }
 
@@ -45,11 +49,26 @@ void prob10_12() {
 
 /*
  * Creates a simulated check
- * users enter the payee and the amount
+ * users enter the date, the name on the check and the amount
  */
 void prob10_19() {
+    string date;
+    cout << "Enter the date: ";
+    cin >> date;
+    
     string name;
-    cout << "Enter name of the payee"
+    cout << "Enter name on the check: ";
+    cin.ignore();
+    getline(cin, name);
+    
+    float amnt;
+    do {
+    cout << "Enter the amount, no more than $10,000: ";
+    cin >> amnt;
+    } while (amnt < 0  || amnt > 10000); // amount cannot be negative
+    cout << endl;
+    
+    formatCheck(date, name, amnt);
 }
 
 // used for problem 10.12
@@ -74,4 +93,16 @@ bool isValid(string pswd) {
     }
     
     return false;
+}
+
+void formatCheck(string date, string name, float amnt) {
+    string amntTxt = numToStr(amnt);
+    cout << setw(60) << right << date << endl << endl
+         << left << "Pay to the order of: " << name << '\t' << amnt << endl
+         << amntTxt;
+}
+
+string numToStr(float amnt) {
+    return "The numToStr function was called\n";
+    
 }
