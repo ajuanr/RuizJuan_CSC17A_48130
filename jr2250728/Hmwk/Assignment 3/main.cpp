20b3a8e0231ff3777597835ef85df967f6e262c2
Juan Ruiz <j.ruiz3750@gmail.com>
2014-10-14 09:38:51 -0700

    Signed-off-by: Juan Ruiz <j.ruiz3750@gmail.com>

--- jr2250728/Hmwk/Assignment 3/main.cpp
+++ jr2250728/Hmwk/Assignment 3/main.cpp
@@ -3,19 +3,56 @@
 */
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
+/* Sructures are here */
+
 // structure for problem 11.1
-struct MovieData {
+struct Movie {
     string title;
     string director;
     string released;
     string length; // the running time
 };
 
+/*
+ * Prototypes begin here
+ */
+
+void prob11_1();
+void prntMov(const Movie&);
 
 int main() {
-
+    prob11_1();
 
     return 0;
 } 
+// Create a structure holding movie data
+// Create two movies
+// Print the movie data
+ void prob11_1() {
+    Movie m1, m2;
+    m1.title = "title of 1";
+    m1.director = "director of 1";
+    m1.released = "release date of 1";
+    m1.length = "length of 1";
+    
+    m2.title = "title of 2";
+    m2.director = "director of 2";
+    m2.released = "release date of 2";
+    m2.length = "length of 2";
+    
+    prntMov(m1);
+    cout << endl;
+    prntMov(m2);
+    
+}
+ 
+ void prntMov(const Movie &m) {
+     cout << setw(14) << left << "Title: " << m.title << endl
+          << setw(14) << left  << "Director: " << m.director << endl
+          << "Release Date: " << m.length << endl
+          << "Running Time: " << m.length << endl;
+     
+ }
