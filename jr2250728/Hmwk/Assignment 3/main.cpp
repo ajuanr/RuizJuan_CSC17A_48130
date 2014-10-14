43c8f90296e02cc2d1db11e6644163df3b4b9def
Juan Ruiz <j.ruiz3750@gmail.com>
2014-10-14 09:24:51 -0700

    Signed-off-by: Juan Ruiz <j.ruiz3750@gmail.com>

--- jr2250728/Hmwk/Assignment 3/main.cpp
+++ jr2250728/Hmwk/Assignment 3/main.cpp
@@ -1,15 +1,21 @@
 /*
 *    Ruiz, Juan - Assignment 3 - 48130
 */
-
-
 #include <iostream>
-
+#include <string>
 using namespace std;
+
+// structure for problem 11.1
+struct MovieData {
+    string title;
+    string director;
+    string released;
+    string length; // the running time
+};
 
 
 int main() {
 
 
-	return 0;
+    return 0;
 } 
