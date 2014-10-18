f93752f250e8bf9f58b718340ed886f2cdfe6f87
Juan Ruiz <j.ruiz3750@gmail.com>
2014-10-18 10:26:54 -0700

    Signed of by Juan Ruiz

--- jr2250728/Midterm/Midterm/Midterm/main.cpp
+++ jr2250728/Midterm/Midterm/Midterm/main.cpp
@@ -8,8 +8,61 @@
 
 #include <iostream>
 
+void prob5();
+
+
+template<class T>
+T factorial(T);
+
+template<class T>
+T maxFac(T);
+
+
+using namespace std;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    //prob5();
+    float f=33;
+    cout << factorial(f) << endl;
+    cout << factorial(++f) << endl;
+    cout << factorial(3) << endl;
     return 0;
+}
+
+
+void prob5() {
+    unsigned uB;
+    cout << "Largest unsigned byte factorial is: " << maxFac(uB) << endl;
+    signed sB;
+    cout << "Largest signed byte factorial is: " << maxFac(sB) << endl;
+    unsigned short uS;
+    cout << "Largest unsigned short factorial is: " << maxFac(uS) << endl;
+    signed short sS;
+    cout << "Largest signed short factorial is: " << maxFac(sS) << endl;
+    unsigned int uI;
+    cout << "Largest unsigned int factorial is: " << maxFac(uI) << endl;
+    signed int sI;
+    cout << "Largest signed int factorial is: " << maxFac(sI) << endl;
+    float fl;
+    cout << "Largest float factorial is: " << maxFac(fl) << endl;
+    
+}
+
+
+// Function calculates factorials
+template<class T>
+T factorial(T n) {
+    if (n==0)
+        return 1;
+    return n*factorial(n-1);
+}
+
+template<class T>
+T maxFac(T) {
+    T n=2; // can't use 1, factorial(1) == factorial(0)
+    while (factorial(n-1) < factorial(n)) {
+        ++n;
+    }
+    // current n overflows go back one
+    return --n;
 }
