34683a20d12c1fdc558803c1389a738e0190b679
Juan Ruiz <j.ruiz3750@gmail.com>
2014-10-18 12:08:59 -0700

    Signed off by Juan Ruiz

--- jr2250728/Midterm/Midterm/Midterm/main.cpp
+++ jr2250728/Midterm/Midterm/Midterm/main.cpp
@@ -7,10 +7,33 @@
 //
 
 #include <iostream>
+#include <string>
 
-void prob5();
+using namespace std;
+
+/*
+ * Structures go here
+*/
+struct Customer {
+    int acctNum;     // holds account number
+    int balance;      // holds balance at beginning of month
+    int chkTotal;     // total of all checks written this month
+    int deposits;     // total of all deposits credited this month
+};
 
 
+/*
+ *  Menu and problemlem protoypes
+ */
+
+void problem1();
+void problem5();
+
+
+
+/*
+ * Functions for problem 1
+ */
 template<class T>
 T factorial(T);
 
@@ -18,15 +41,24 @@ template<class T>
 T maxFac(T);
 
 
-using namespace std;
+/*
+ * Functions for problem 2
+ */
+void AcctInfo(Customer *);
+void read(Customer *, int);
+
 
 int main(int argc, const char * argv[]) {
-    prob5();
+    //problem5();
     return 0;
 }
 
+void problem1() {
+    cout << "You called problem 1\n";
+}
 
-void prob5() {
+
+void problem5() {
     unsigned uB;
     cout << "Largest unsigned byte factorial is: " << maxFac(uB) << endl;
     signed sB;
@@ -61,4 +93,31 @@ T maxFac(T) {
     }
     // current n overflows go back one
     return --n;
+}
+
+void AcctInfo(Customer *c) {
+    bool valid = false;
+    do {
+        cout << "Enter the account number: ";
+        cin >> c->acctNum;
+        // if account number is more than 5 digits
+        // this result will be >= to 1
+        if (c->acctNum / 100000 < 1)
+            valid=true;
+    } while (!valid);
+    
+    int n;
+    do {
+        cout << "Enter"
+    }
+}
+
+
+// function reads in checks or deposits depending on type
+// type should be 1 or -1
+// if -1, then a check was written and it will be subtracted from balance
+// otherwise add to balance
+void read(Customer *c, int type ) {
+    int num;
+    c->balance += (num * type);
 }
