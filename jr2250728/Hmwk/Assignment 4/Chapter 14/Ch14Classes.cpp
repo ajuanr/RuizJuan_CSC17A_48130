49640c6de175fcea250396e4f52215292b9286e6
Juan Ruiz <j_ruiz05@yahoo.com>
2014-11-12 09:36:31 -0800

    Signed off by Juan Ruiz

--- jr2250728/Hmwk/Assignment 4/Chapter 14/Ch14Classes.cpp
+++ jr2250728/Hmwk/Assignment 4/Chapter 14/Ch14Classes.cpp
@@ -7,3 +7,34 @@
 //
 
 #include "Ch14Classes.h"
+
+Numbers::Numbers(int n) {
+    // make sure number entered is not negative
+    (n>0) ? num = n : num=0;
+}
+
+void Numbers::print() const {
+    
+}
+
+int* Numbers::numArray() const{
+    int size = 0;
+    int numCpy = num;
+    while ( numCpy != 0 ) {
+        numCpy/=10;
+        size++;
+    }
+    // reset the copy of num to store the values in an array
+    numCpy = num;
+    int* array = new int[size];
+    
+    // pick of the values and place them into the array
+    // starting at the end of the array
+    while (numCpy != 0) {
+        // pick of the last number and place it into the end of the array
+        
+    }
+    
+    
+    return array;
+}
--- jr2250728/Hmwk/Assignment 4/Chapter 14/Ch14Classes.h
+++ jr2250728/Hmwk/Assignment 4/Chapter 14/Ch14Classes.h
@@ -10,8 +10,33 @@
 #define __Chapter14__Ch14Classes__
 
 /**********************************************
- **********Chapter 14 - Problem 1**************
- **********************************************
+ ********* Chapter 14 - Problem 1 *************
  *********************************************/
+// convert integers to their english description
+// e.g. 100 = one hundred
+class Numbers {
+public:
+    Numbers(int);
+    void print() const;
+    //  Static variables. As specified in text
+    static char lessThan20 [20][25];
+    static char hundred[];
+    static char thousand[];
+    
+private:
+    int num;
+    
+    // utitiliy functions
+    int *numArray() const;
+};
+
+// static var definitions
+char Numbers::lessThan20[20][25]  = {"one", "two", "three", "four", "five",
+                                     "six", "seven", "eight", "nine", "ten",
+                                     "eleven", "twelve", "thirteen", "fourteen",
+                                     "fifteen", "sixteen", "seventeen",
+                                     "eighteen", "nineteen"};
+char Numbers::hundred[] = {"hundred"};
+char Numbers::thousand[] = {"thousand"};
 
 #endif /* defined(__Chapter14__Ch14Classes__) */
