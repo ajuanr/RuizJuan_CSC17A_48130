802b35b5862fcc9f13393a20e62d109881182f3d
Juan Ruiz <j_ruiz05@yahoo.com>
2014-11-21 15:02:59 -0800

    Signed off by Juan Ruiz

---
+++ jr2250728/Proj/Project 2/Minesweeper.cpp
@@ -0,0 +1,9 @@
+//
+//  Minesweeper.cpp
+//  Project 2
+//
+//  Created by Juan Ruiz on 11/21/14.
+//  Copyright (c) 2014 Juan Ruiz. All rights reserved.
+//
+
+#include "Minesweeper.h"
---
+++ jr2250728/Proj/Project 2/Minesweeper.h
@@ -0,0 +1,14 @@
+//
+//  Minesweeper.h
+//  Project 2
+//
+//  Created by Juan Ruiz on 11/21/14.
+//  Copyright (c) 2014 Juan Ruiz. All rights reserved.
+//
+
+#ifndef __Project_2__Minesweeper__
+#define __Project_2__Minesweeper__
+
+#include <stdio.h>
+
+#endif /* defined(__Project_2__Minesweeper__) */
