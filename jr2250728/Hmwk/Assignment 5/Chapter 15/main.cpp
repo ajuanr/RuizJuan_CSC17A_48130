a553f7f6e51eccb602f42e7a8608ae406b0e5c54
Juan Ruiz <j_ruiz05@yahoo.com>
2014-11-21 19:48:46 -0800

    Signed off by Juan Ruiz

---
+++ jr2250728/Hmwk/Assignment 5/Chapter 15/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chapter 15
+//
+//  Created by Juan Ruiz on 11/21/14.
+//  Copyright (c) 2014 Juan Ruiz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
