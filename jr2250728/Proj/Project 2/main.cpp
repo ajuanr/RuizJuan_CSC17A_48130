d6a9e351f5a1ef349113dfcb17e6150a2b4fa4b4
Juan Ruiz <j.ruiz3750@gmail.com>
2014-12-08 19:08:30 -0800

    Signed off by Juan Ruiz

--- jr2250728/Proj/Project 2/main.cpp
+++ jr2250728/Proj/Project 2/main.cpp
@@ -3,28 +3,7 @@
  *
  * Project allows user to play Minesweeper
  * 
- * Structures
- *      Functions with structure input: Most of the functions
- *      Functions returning structures: Function create()      Line 235
- *      Function with array of structures: Function fields()   Line 584
- *
- * Memory allocation:
- *      dynamic 2D array in function create()                  Line 242
- *      dynamic 1D array in function userName()                Line 225
- *
- * Binary Files
- *      Writing to: function writeBin()                        Line 553
- *      Reading from: function readBin()                       Line 562
- * Strings
- *      Function:
- *          writeBin(), readBin(), userName()               Line 553, 562, 220
- *
- * Pointers
- *      Structure passed as pointer in most functions
- *      2D array pointer notation in prntClear()               Line 288, 290
- *      1D array pointer notation in userName()                 Line 227, 229
- *      Returning pointer in create()                           Line 235
- */
+*/
 
 #include <iostream>
 #include <cstdlib>
